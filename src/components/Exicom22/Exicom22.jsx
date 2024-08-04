5185a8e0695a40092dc6ac8f552e11e3460f4656
jubuhan <jubuhantt5203@gmail.com>
2024-08-04 21:01:02 +0530

    exicom23and22

---
+++ src/components/Exicom22/Exicom22.jsx
@@ -0,0 +1,37 @@
+
+import Heading from "../Heading"
+import sb22 from "../../assets/images/Exicom22/exc22SB.webp"
+import cs_ias22 from "../../assets/images/Exicom22/exc22CsIAs.webp"
+import com_wig22 from "../../assets/images/Exicom22/exc22WigCOMS.webp"
+import Banner from "../Banner/Banner"
+const Execom22 = () => {
+
+    return (
+      <>
+        <Banner />
+        <div className="container">
+          <Heading text="EXECOM'22" />
+          <div className="flex flex-wrap flex-row justify-between items-center">
+            <img
+              src={sb22}
+              alt="execom22"
+              className="w-full md:w-1/4 m-2 rounded-lg shadow-lg"
+            />
+            <img
+              src={cs_ias22}
+              alt="execom22"
+              className="w-full md:w-1/4 m-2 rounded-lg shadow-lg"
+            />
+            <img
+              src={com_wig22}
+              alt="execom22"
+              className="w-full md:w-1/4 m-2 rounded-lg shadow-lg"
+            />
+          </div>
+        </div>
+      </>
+    );
+
+}
+
+export default Execom22
