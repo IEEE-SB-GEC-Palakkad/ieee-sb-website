ff14aa37240d6f7d532ec6158706df409ab8cc88
jubuhan <jubuhantt5203@gmail.com>
2024-08-19 01:06:52 +0530

    societies and exicom 24

--- src/components/Socities/Societies.jsx
+++ src/components/Socities/Societies.jsx
@@ -9,6 +9,39 @@ import Heading from "../Heading";
 import SocietyCard from "../SocietyCard";
 
 const Societies = () => {
+  const societiesData = [
+    { 
+      image: cs, 
+      name: "Computer Society", 
+      link1: "https://www.linkedin.com/company/ieee-cs-sbc-gec-palakkad/", 
+      link2: "https://www.instagram.com/ieeecsgecpkd?igsh=a3k4ZXdwenZrYXdt"
+    },
+    { 
+      image: com, 
+      name: "Communications Society", 
+      link1: "https://www.linkedin.com/company/ieee-ias-sb-chapter-gec-palakkad/", 
+      link2: "https://www.instagram.com/ieeecomsocgecpalakkad?igsh=MXh1Z3Blc3AxbWU0cw=="
+    },
+    { 
+      image: ias, 
+      name: "IAS Society", 
+      link1: "https://www.linkedin.com/company/ieee-ias-sb-chapter-gec-palakkad/", 
+      link2: "https://www.instagram.com/ieeeiasgecpkd?igsh=MXFqdDl4M3E5a25naw=="
+    },
+    { 
+      image: wie, 
+      name: "WIE", 
+      link1: "https://www.linkedin.com/company/ieee-wie-sb-chapter-gec-palakkad/", 
+      link2: "https://www.instagram.com/ieeewiegecpalakkad?igsh=eWRvdGpnd3Z2N2J5"
+    },
+    { 
+      image: sight, 
+      name: "SIGHT", 
+      link1: "https://www.linkedin.com/company/ieee-sight-sb-chapter-gec-palakkad/", 
+      link2: "https://www.instagram.com/ieeesightgecpkd?igsh=MXRxdHY1djVqOXlhbA=="
+    },
+  ];
+
   return (
     <>
       <div id="society" className="societies-section relative container">
@@ -18,11 +51,15 @@ const Societies = () => {
         </div>
 
         <div className="socities-list flex flex-wrap flex-row justify-center items-center">
-          <SocietyCard image={cs} name="Computer Society" />
-          <SocietyCard image={com} name="Communications Society" />
-          <SocietyCard image={ias} name="IAS Society" />
-          <SocietyCard image={wie} name="WIE" />
-          <SocietyCard image={sight} name="SIGHT" />
+          {societiesData.map((society, index) => (
+            <SocietyCard 
+              key={index}
+              image={society.image} 
+              name={society.name}
+              link1={society.link1}
+              link2={society.link2}
+            />
+          ))}
         </div>
       </div>
     </>
