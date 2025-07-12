51babd720d9405ca95136c78a355a5c67f8b79e4
Gauiri Shankar G Nair <sgauiri@gmail.com>
2025-07-12 21:35:30 +0530

    Update Excelsior.jsx

--- src/components/Events/Excelsior.jsx
+++ src/components/Events/Excelsior.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import eventData from "../../helpers/events.json";
 import Heading from "../Heading";
-/*import "./SymposiumPage.css";
+import "./Excelsior.css";
 
-const SymposiumPage = () => {
+const Excelsior = () => {
   const navigate = useNavigate();
   const symposiumData = eventData[0][0];*/
   
